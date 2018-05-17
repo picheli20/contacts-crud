072caf1784d63fa79ebf4c0971f7f1c871fcbd92
Fabio Picheli <contato@fabiopicheli.com>
2018-05-17 13:29:00 +0200

    feat: add support to heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const expressGraphql = require('express-graphql');
+const path = require('path');
+const compression = require('compression');
 const { buildSchema } = require('graphql');
 const { schema, country } = require('./country/country.graphql');
+const port = process.env.PORT || 4000;
 
 // Create an express server and a GraphQL endpoint
 var app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+app.use(compression()) //compressing dist folder
 
 app.use('/graphql', expressGraphql({
     schema: buildSchema(schema),
@@ -12,4 +18,8 @@ app.use('/graphql', expressGraphql({
     graphiql: true
 }));
 
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.listen(port, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
